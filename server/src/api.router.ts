24c0a168a205c25c0a3bb5a688e1809636c9fd7d
Molnár Kristóf Ferenc <molnar.kristof@e-corvina.hu>
2024-10-06 00:53:36 +0200

    Scene metadata

--- server/src/api.router.ts
+++ server/src/api.router.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import targetsController from "./controllers/targets.controller";
 import tleController from "./controllers/tle.controller";
+import sceneMetadataController from "./controllers/sceneMetadata,controller";
 
 
 const router = express.Router();
@@ -9,6 +10,7 @@ const router = express.Router();
 // --- Targets ---
 router.use("/targets", targetsController)
 router.use("/tle", tleController)
+router.use("/scene-metadata/", sceneMetadataController)
 
 
 
---
+++ server/src/controllers/sceneMetadata,controller.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import {TLEService} from "../services/tle.service";
+import {SceneMetadata} from "../models/sceneMetadata.model";
+
+
+const router = express.Router();
+
+
+router.get("/:id", async (req, res) => {
+
+	try{
+		const metadatas = await SceneMetadata.findOne().exec();
+		res.status(200).send(metadatas);
+	}catch (e){
+		res.status(500).send(e);
+	}
+})
+
+
+export default router;
